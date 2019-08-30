444fa31ebd97dd8084a8fc677705fec8fea33bc9
Atsushi Okada <atsushii@aqua.ocn.ne.jp>
2019-08-30 15:40:54 +0900

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slottest02
+//
+//  Created by Atsushi Okada on 2019/08/29.
+//  Copyright © 2019 Atsushi Okada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
